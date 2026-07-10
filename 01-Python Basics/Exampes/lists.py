8b8868b7c5367c9a30acfba11d03e61db44f96c0
Hesam Pourabbasain <hesampourabbasain@MacBookAir.bwrouter>
2026-07-10 11:15:59 +0330

    Added lists.py2  to examples

--- 01-Python Basics/Exampes/lists.py
+++ 01-Python Basics/Exampes/lists.py
@@ -1,6 +1,6 @@
-# fruits = ["banana", "Apple", "Grape"]
-# print (fruits[2])
+# # fruits = ["banana", "Apple", "Grape"]
+# # print (fruits[2])
 
 
-vehicle = ["Car", "Bicycle", "Plane"]
-print (vehicle [0:2])
+# vehicle = ["Car", "Bicycle", "Plane"]
+# print (vehicle [0:2]) 
