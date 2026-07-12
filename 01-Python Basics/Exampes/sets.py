69ee5434f188b4609661f209121b6fa14c15247f
HesamPourabbasian <hesampourabbasian6@gmail.com>
2026-07-12 13:09:20 +0330

    added setes.py to basics

---
+++ 01-Python Basics/Exampes/sets.py
@@ -0,0 +1,6 @@
+fruits = {"apple", "banana"}
+
+fruits.add("cherry")          # add a single element
+fruits.update(["mango", "kiwi"])  # add multiple elements (list, set, tuple, etc.)
+
+print(fruits)  
