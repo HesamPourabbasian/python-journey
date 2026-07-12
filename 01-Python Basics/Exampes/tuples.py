a46849f690252ce559a8f985cbc0f75071b0378c
HesamPourabbasian <hesampourabbasian6@gmail.com>
2026-07-12 12:45:56 +0330

    edite tuples.py to basic examples

--- 01-Python Basics/Exampes/tuples.py
+++ 01-Python Basics/Exampes/tuples.py
@@ -1,8 +1,14 @@
-fruits = ("apple", "banana", "cherry")
+# fruits = ("apple", "banana", "cherry")
 
-for fruit in fruits:
-    print(fruit)
+# for fruit in fruits:
+#     print(fruit)
 
-# With index
-for i, fruit in enumerate(fruits):
-    print(i, fruit)
+# # With index
+# for i, fruit in enumerate(fruits):
+#     print(i, fruit)
+
+numbers = (1, 2, 3, 4, 5)
+first, *middle, last = numbers
+print(first)    # 1
+print(middle)   # [2, 3, 4]
+print(last)     # 5
